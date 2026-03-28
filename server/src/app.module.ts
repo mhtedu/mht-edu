df9874e40f2d919eb46afaa72c8705f7fca50de8
user6143661237 <2156599318681536-user6143661237@noreply.coze.cn>
2026-03-28 21:44:30 +0800

    fix: 修复 AppModule 注册 AppController 和 AppService
    问题：/api/hello 和 /api/health 返回404
    原因：AppModule 没有注册 AppController 和 AppService
    解决方案：
    1. 添加 AppController 到 controllers
    2. 添加 AppService 到 providers
    3. 用户需要在服务器上更新代码并重新编译

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { OrderModule } from './modules/order/order.module';
 import { PaymentModule } from './modules/payment/payment.module';
@@ -35,5 +37,7 @@ import { ReferralLockModule } from './modules/referral-lock/referral-lock.module
     EliteClassModule,
     ReferralLockModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
